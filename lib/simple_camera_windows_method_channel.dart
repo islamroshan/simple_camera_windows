d3483af307d4338324437d81a7a56aa2c87f5b88
islamroshan <islamroshan103@mail.com>
2024-08-07 15:32:44 +0500

    MethodChannel Fixed

--- lib/simple_camera_windows_method_channel.dart
+++ lib/simple_camera_windows_method_channel.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
 import 'simple_camera_windows_platform_interface.dart';
 
 /// An implementation of [SimpleCameraWindowsPlatform] that uses method channels.
 class MethodChannelSimpleCameraWindows extends SimpleCameraWindowsPlatform {
   /// The method channel used to interact with the native platform.
-  @visibleForTesting
   final methodChannel = const MethodChannel('dev.spantik/camera');
 
   @override
--- lib/simple_camera_windows_platform_interface.dart
+++ lib/simple_camera_windows_platform_interface.dart
@@ -5,39 +5,32 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'simple_camera_windows_method_channel.dart';
 
 abstract class SimpleCameraWindowsPlatform extends PlatformInterface {
-  /// Constructs a SimpleCameraWindowsPlatform.
   SimpleCameraWindowsPlatform() : super(token: _token);
 
   static final Object _token = Object();
 
   static SimpleCameraWindowsPlatform _instance = MethodChannelSimpleCameraWindows();
 
-  /// The default instance of [SimpleCameraWindowsPlatform] to use.
-  ///
-  /// Defaults to [MethodChannelSimpleCameraWindows].
   static SimpleCameraWindowsPlatform get instance => _instance;
 
-  /// Platform-specific implementations should set this with their own
-  /// platform-specific class that extends [SimpleCameraWindowsPlatform] when
-  /// they register themselves.
   static set instance(SimpleCameraWindowsPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
   }
 
   Future<void> initializeCamera() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+    throw UnimplementedError('initializeCamera() has not been implemented.');
   }
 
   Future<void> startCamera() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+    throw UnimplementedError('startCamera() has not been implemented.');
   }
 
   Future<void> stopCamera() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+    throw UnimplementedError('stopCamera() has not been implemented.');
   }
 
   Future<Uint8List?> captureFrame() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+    throw UnimplementedError('captureFrame() has not been implemented.');
   }
 }
